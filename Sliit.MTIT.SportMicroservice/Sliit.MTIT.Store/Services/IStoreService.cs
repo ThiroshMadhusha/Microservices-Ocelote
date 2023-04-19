ed02ba91d3aeb937a9074cf99bec54d27a08eb33
Jayathunga T.M <thiroshmadhusha0520@gmail.com>
2023-04-20 00:35:06 +0530

    create store microservices
    Co-Authored-By: it20254520 <89467299+it20254520@users.noreply.github.com>

---
+++ Sliit.MTIT.SportMicroservice/Sliit.MTIT.Store/Services/IStoreService.cs
@@ -0,0 +1,15 @@
+﻿namespace Sliit.MTIT.Store.Services
+{
+    public interface IStoreService
+    {
+        List<Models.Store> GetStores();
+
+        Models.Store? GetStore(int id);
+
+        Models.Store? AddStore(Models.Store store);
+
+        Models.Store? UpdateStore(Models.Store store);
+
+        bool? DeleteStore(int id);
+    }
+}
---
+++ Sliit.MTIT.SportMicroservice/Sliit.MTIT.Store/Services/StoreService.cs
@@ -0,0 +1,49 @@
+﻿using Sliit.MTIT.Store.Data;
+using Sliit.MTIT.Store.Models;
+
+namespace Sliit.MTIT.Store.Services
+{
+    public class StoreService : IStoreService
+    {
+        public List<Models.Store> GetStores()
+        {
+            return StoreMockDataService.Stores;
+        }
+
+        public Models.Store? GetStore(int id)
+        {
+            return StoreMockDataService.Stores.FirstOrDefault(x => x.Id == id);
+        }
+
+        public Models.Store? AddStore(Models.Store store)
+        {
+            StoreMockDataService.Stores.Add(store);
+            return store;
+        }
+
+        public Models.Store? UpdateStore(Models.Store store)
+        {
+            Models.Store selectedStore = StoreMockDataService.Stores.FirstOrDefault(x => x.Id == store.Id);
+            if (selectedStore != null)
+            {
+                selectedStore.ItemName = store.ItemName;
+                selectedStore.ItemQuantity = store.ItemQuantity;
+                selectedStore.StoreName = store.StoreName;
+                return selectedStore;
+            }
+
+            return selectedStore;
+        }
+
+        public bool? DeleteStore(int id)
+        {
+            Models.Store selectedStore = StoreMockDataService.Stores.FirstOrDefault(x => x.Id == id);
+            if (selectedStore != null)
+            {
+                StoreMockDataService.Stores.Remove(selectedStore);
+                return true;
+            }
+            return false;
+        }
+    }
+}

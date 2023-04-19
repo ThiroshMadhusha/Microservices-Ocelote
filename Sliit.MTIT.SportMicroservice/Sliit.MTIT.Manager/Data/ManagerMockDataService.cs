4ff4daa215100bf89be406bd33f0d6a88c869003
Jayathunga T.M <thiroshmadhusha0520@gmail.com>
2023-04-19 23:44:24 +0530

    Create Manager Microservice
    Co-Authored-By: Semini J.P.D.L. <89403448+it20241346@users.noreply.github.com>

---
+++ Sliit.MTIT.SportMicroservice/Sliit.MTIT.Manager/Data/ManagerMockDataService.cs
@@ -0,0 +1,15 @@
+﻿namespace Sliit.MTIT.Manager.Data
+{
+    public static class ManagerMockDataService
+    {
+        public static List<Models.Manager> Managers = new List<Models.Manager>()
+        {
+            new Models.Manager { Id=1, ManagerName = "Thirosh", BranchName = "TDSM 1", Location = "Galle"},
+            new Models.Manager { Id=2, ManagerName = "Sachini", BranchName = "TDSM 2", Location = "Kandy"},
+            new Models.Manager { Id=3, ManagerName = "Kasuni", BranchName = "TDSM 3", Location = "Jafna"},
+            new Models.Manager { Id=4, ManagerName = "Apsara", BranchName = "TDSM 4", Location = "Colombo"},
+            new Models.Manager { Id=5, ManagerName = "Nethuki", BranchName = "TDSM 5", Location = "Gampaha"},
+
+        };
+    }
+}
